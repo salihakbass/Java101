73ea5be64f85528db691842f677a1d16945520ad
Salih Akbas <salihhakbass@gmail.com>
2023-04-17 02:23:59 +0300

    third homework done

---
+++ app/src/main/java/com/salihakbas/java101/HipotenüsBulma.java
@@ -0,0 +1,22 @@
+package com.salihakbas.java101;
+
+import java.util.Scanner;
+
+public class HipotenüsBulma {
+    public static void main(String[] args) {
+        int a, b;
+        double c;
+
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("A kenarını giriniz: ");
+        a = girdi.nextInt();
+        System.out.print("B kenarını giriniz: ");
+        b = girdi.nextInt();
+
+        c = Math.sqrt((a*a) + (b*b));
+
+
+        System.out.println("Hipotenüs: " + c);
+
+    }
+}
