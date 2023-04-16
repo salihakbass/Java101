13598a2ae9b234883e0f20e3cf4ec3a6b7b0d739
Salih Akbas <salihhakbass@gmail.com>
2023-04-17 02:33:56 +0300

    fourth homework

--- app/src/main/java/com/salihakbas/java101/TaksimetreHesaplama.java
+++ app/src/main/java/com/salihakbas/java101/TaksimetreHesaplama.java
@@ -1,2 +1,22 @@
-package com.salihakbas.java101;public class TaksimetreHesaplama {
+package com.salihakbas.java101;
+
+import java.util.Scanner;
+
+public class TaksimetreHesaplama {
+    public static void main(String[] args) {
+        int km, minimumTutar = 20, açılısTutarı = 10;
+        double toplamTutar, kmBasıTutar = 2.20;
+
+        Scanner mesafe = new Scanner(System.in);
+        System.out.print("Mesafeyi giriniz: ");
+        km = mesafe.nextInt();
+
+        toplamTutar = km * kmBasıTutar;
+        toplamTutar += açılısTutarı;
+        System.out.println(toplamTutar <= 20 ? "Tutarınız: " + minimumTutar + "TL" : "Tutarınız: " + toplamTutar + "TL");
+
+
+
+
+    }
 }
