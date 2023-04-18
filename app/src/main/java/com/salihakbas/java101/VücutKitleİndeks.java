644e27d7341a77186f1156d48ba4017de59ee498
Salih Akbas <salihhakbass@gmail.com>
2023-04-18 22:51:06 +0300

    last homework done

---
+++ app/src/main/java/com/salihakbas/java101/VücutKitleİndeks.java
@@ -0,0 +1,18 @@
+package com.salihakbas.java101;
+
+import java.util.Scanner;
+
+public class VücutKitleİndeks {
+    public static void main(String[] args) {
+        double boy, kilo;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Boyunuzu giriniz: ");
+        boy = input.nextDouble();
+        System.out.print("Kilonuzu giriniz: ");
+        kilo = input.nextDouble();
+
+        double vki = kilo / (boy * boy);
+        System.out.println("Vücut Kitle İndeksiniz: " + vki);
+    }
+}
