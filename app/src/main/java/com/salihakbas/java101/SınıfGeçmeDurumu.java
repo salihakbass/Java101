a1311c9a18f668bb3a9987911766b59f02bb009d
Salih Akbas <salihhakbass@gmail.com>
2023-04-22 19:32:15 +0300

    homework done

---
+++ app/src/main/java/com/salihakbas/java101/SınıfGeçmeDurumu.java
@@ -0,0 +1,37 @@
+package com.salihakbas.java101;
+
+import java.util.Scanner;
+
+public class SınıfGeçmeDurumu {
+    public static void main(String[] args) {
+        int matematik, türkce, fizik, kimya, müzik;
+
+        Scanner notlar = new Scanner(System.in);
+
+        System.out.print("Matematik notunuz: ");
+        matematik = notlar.nextInt();
+
+        System.out.print("Türkçe notunuz: ");
+        türkce = notlar.nextInt();
+
+        System.out.print("Fizik notunuz: ");
+        fizik = notlar.nextInt();
+
+        System.out.print("Kimya notunuz: ");
+        kimya = notlar.nextInt();
+
+        System.out.print("Müzik notunuz: ");
+        müzik = notlar.nextInt();
+
+        double ortalama = (matematik + türkce + fizik + kimya + müzik) / 5.0 ;
+
+            if(ortalama < 55) {
+                System.out.println("Kaldınız, ortalamanız: " + ortalama);
+            }else{
+                System.out.println("Geçtiniz, ortalamanız: " + ortalama);
+            }
+
+
+
+    }
+}
