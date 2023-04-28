0c53d5acbc5b10590d7271c25390aebbbff42fd5
Salih Akbas <salihhakbass@gmail.com>
2023-04-28 22:52:33 +0300

    homework done

---
+++ app/src/main/java/com/salihakbas/java101/ArtıkYılHesaplama.java
@@ -0,0 +1,20 @@
+package com.salihakbas.java101;
+
+import java.util.Scanner;
+
+public class ArtıkYılHesaplama {
+    public static void main(String[] args) {
+        int yıl;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Yıl giriniz: ");
+        yıl = input.nextInt();
+
+        if(yıl % 4 == 0 || yıl % 400 == 0) {
+            System.out.println(yıl + " " + "bir artık yıldır");
+        }else{
+            System.out.println(yıl + " " + "bir artık yıl değildir");
+        }
+    }
+
+}
