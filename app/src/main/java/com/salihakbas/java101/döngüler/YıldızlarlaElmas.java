7734167ddf089073fba34cd36b0ca0c0e3815600
Salih Akbas <salihhakbass@gmail.com>
2023-05-08 12:49:12 +0300

    homework done

---
+++ app/src/main/java/com/salihakbas/java101/döngüler/YıldızlarlaElmas.java
@@ -0,0 +1,30 @@
+package com.salihakbas.java101.döngüler;
+
+import java.util.Scanner;
+
+public class YıldızlarlaElmas {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Basamak sayısını giriniz: ");
+        int n = input.nextInt();
+
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j < (n- i); j++) {
+                System.out.print(" ");
+            }
+            for(int k = 1; k <= (2 *i -1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for(int i = n; i >= 0; i-- ){
+            for(int j = (n- i); j > 0; j-- ){
+                System.out.print(" ");
+            }
+            for(int k = (2 * i - 1); k > 0; k--){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+}
