2c5743d988c615a5e7b66b9cdf0bc5f69ee6580b
Salih Akbas <salihhakbass@gmail.com>
2023-05-10 14:39:50 +0300

    homework done

---
+++ app/src/main/java/com/salihakbas/java101/metotlar/PalindromSayı.java
@@ -0,0 +1,19 @@
+package com.salihakbas.java101.metotlar;
+
+public class PalindromSayı {
+
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNum = 0, lastNum;
+
+        while(temp != 0) {
+            lastNum = temp % 10;
+            reverseNum = (reverseNum * 10) + lastNum;
+            temp /= 10;
+        }
+        if(number == reverseNum) return true;
+        else return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(1001));
+    }
+}
